000be127ecb51e7e02bcd1236b6b61e222aca0c8
a_srinibash <asrinibashachary@gmail.com>
2023-05-27 01:36:41 +0530

    Initial commit

---
+++ Hospital Management System/src/Management/ConnectDB.java
@@ -0,0 +1,21 @@
+package Management;
+
+import java.sql.*;
+
+public class ConnectDB {
+    Connection con;
+   Statement s;
+    public ConnectDB () {
+        try { 
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/hospital_database";
+            String username = "root";
+            String password = "srinu123";
+            con = DriverManager.getConnection(url, username, password);
+            s=con.createStatement();
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
+}
+
